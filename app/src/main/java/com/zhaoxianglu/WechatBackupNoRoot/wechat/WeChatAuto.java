354aa904c6ce5b4be8c90a4f7be6862f60064f1a
ZhaoXiangLu <105914203+MiDuoKi@users.noreply.github.com>
2023-02-04 23:10:27 +0800

    - add

--- app/src/main/java/com/zhaoxianglu/WechatBackupNoRoot/wechat/WeChatAuto.java
+++ app/src/main/java/com/zhaoxianglu/WechatBackupNoRoot/wechat/WeChatAuto.java
@@ -1,19 +1,5 @@
 package com.zhaoxianglu.WechatBackupNoRoot.wechat;
 
-import android.graphics.Rect;
-import android.util.Log;
-import android.view.accessibility.AccessibilityNodeInfo;
-
-import com.blankj.utilcode.util.ScreenUtils;
-import com.zhaoxianglu.WechatBackupNoRoot.utils.AutoUtils;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static com.zhaoxianglu.WechatBackupNoRoot.utils.AutoUtils.SLIDE_DOWN;
-
-
 public class WeChatAuto {
 
     private static String TAG = "WeChatAuto";
