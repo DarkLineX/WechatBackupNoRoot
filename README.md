1d8155b98e928ad0ca91d39fc1787a6120a3fae6
ZhaoXiangLu <105914203+MiDuoKi@users.noreply.github.com>
2023-02-22 19:05:22 +0800

    - add

--- README.md
+++ README.md
@@ -36,24 +36,24 @@
 #### 微信8.0
 
 :white_check_mark: Version 8.0.32 
-:x: Version 8.0.31 
-:x: Version 8.0.30 
-:x: Version 8.0.28 
-:x: Version 8.0.27
+:white_check_mark: Version 8.0.31 
+:white_check_mark: Version 8.0.30 
+:white_check_mark: Version 8.0.28 
+:white_check_mark: Version 8.0.27
 
-:x: Version 8.0.25 
-:x: Version 8.0.24 
-:x: Version 8.0.23 
-:x: Version 8.0.22 
+:white_check_mark: Version 8.0.25 
+:white_check_mark: Version 8.0.24 
+:white_check_mark: Version 8.0.23 
+:white_check_mark: Version 8.0.22 
 :white_check_mark: Version 8.0.21 
 
-:x: Version 8.0.20 
-:x: Version 8.0.19 
-:x: Version 8.0.18 
-:x: Version 8.0.17 
-:x: Version 8.0.16
+:white_check_mark: Version 8.0.20 
+:white_check_mark: Version 8.0.19 
+:white_check_mark: Version 8.0.18 
+:white_check_mark: Version 8.0.17 
+:white_check_mark: Version 8.0.16
 
-:x: Version 8.0.15 
+:white_check_mark: Version 8.0.15 
 :white_check_mark: Version 8.0.14 
 :white_check_mark: Version 8.0.13 
 :white_check_mark: Version 8.0.12 
--- app/src/main/java/com/zhaoxianglu/WechatBackupNoRoot/constant/NodeInfo.java
+++ app/src/main/java/com/zhaoxianglu/WechatBackupNoRoot/constant/NodeInfo.java
@@ -18,49 +18,39 @@ public class NodeInfo {
             case "8.0.7":
             case "8.0.9":
             case "8.0.11":
-                nodeInfo.msg_page_title = "com.tencent.mm:id/ipt";
-                nodeInfo.msg_page_list_view = "com.tencent.mm:id/awv";
-                nodeInfo.msg_page_text_msg_info = "com.tencent.mm:id/auk";
-                break;
+            case "8.0.14":
             case "8.0.10":
                 nodeInfo.msg_page_title = "com.tencent.mm:id/ipt";
                 nodeInfo.msg_page_list_view = "com.tencent.mm:id/awv";
                 nodeInfo.msg_page_text_msg_info = "com.tencent.mm:id/auk";
                 break;
-            case "8.0.14":
-                break;
             case "8.0.15":
-                break;
             case "8.0.16":
+                nodeInfo.msg_page_title = "com.tencent.mm:id/ipv";
+                nodeInfo.msg_page_list_view = "com.tencent.mm:id/awv";
+                nodeInfo.msg_page_text_msg_info = "com.tencent.mm:id/auk";
                 break;
             case "8.0.18":
-                break;
-            case "8.0.19":
-                break;
             case "8.0.20":
-                break;
+            case "8.0.19":
             case "8.0.21":
+            case "8.0.23":
+            case "8.0.22":
                 nodeInfo.msg_page_title = "com.tencent.mm:id/kog";
                 nodeInfo.msg_page_list_view = "com.tencent.mm:id/b79";
                 nodeInfo.msg_page_text_msg_info = "com.tencent.mm:id/b4b";
                 break;
-            case "8.0.22":
-                break;
-            case "8.0.23":
-                break;
             case "8.0.24":
+                nodeInfo.msg_page_title = "com.tencent.mm:id/ko6";
+                nodeInfo.msg_page_list_view = "com.tencent.mm:id/b79";
+                nodeInfo.msg_page_text_msg_info = "com.tencent.mm:id/b4b";
                 break;
             case "8.0.25":
-                break;
-            case "8.0.27":
-                break;
-            case "8.0.28":
-                break;
-            case "8.0.30":
-                break;
-            case "8.0.31":
-                break;
             case "8.0.32":
+            case "8.0.31":
+            case "8.0.27":
+            case "8.0.28":
+            case "8.0.30":
                 nodeInfo.msg_page_title = "com.tencent.mm:id/ko4";
                 nodeInfo.msg_page_list_view = "com.tencent.mm:id/b79";
                 nodeInfo.msg_page_text_msg_info = "com.tencent.mm:id/b4b";
